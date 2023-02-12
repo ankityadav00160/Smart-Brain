bb2474e1b1dbc9c41fb5d794c588472cd63a9b4c
ankityadav00159 <ankityadav00159@gmail.com>
2023-02-12 13:48:06 +0530

    Api Code

--- src/App.js
+++ src/App.js
@@ -20,6 +20,57 @@ class App extends Component {
 
   onButtonSumbit = () =>{
     console.log('click');
+
+    const USER_ID = "d434ce68718343a7a4490ed2e282e405"
+
+    const PAT = "d271ca98cf5e443a9ca40fe686ff3299"
+
+    const APP_ID = "my-first-application"
+
+    const MODEL_ID = "face-detection";
+  const MODEL_VERSION_ID = "45fb9a671625463fa646c3523a3087d5";
+
+  const IMAGE_URL = this.state.input;
+
+  const raw = JSON.stringify({
+    user_app_id:{
+      user_id: USER_ID,
+      app_id: APP_ID,
+    },
+    inputs:[{
+      data:{
+        image:{
+          url:IMAGE_URL,
+        }
+      }
+    }]
+  })
+
+  const requestOptions = {
+    method: "POST",
+    headers:{
+      Accept: "application/json",
+      Authorization: "Key" + PAT,
+    },
+    body: raw,
+  };
+
+  fetch(
+    "https://api.clarifai.com/v2/models/"+
+    MODEL_ID +
+    "/versions/" +
+    MODEL_VERSION_ID +
+    "/outputs",
+    requestOptions
+  )
+
+  .then((response) => response.json())
+  .then((result) =>
+  console.log(result)
+  )
+
+  .catch((error) => console.log("error", error))
+
   }
 
   render(){

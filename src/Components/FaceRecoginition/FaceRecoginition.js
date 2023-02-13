84a21fff9388c171707263f56c4a893ff72c2b7d
ankityadav00159 <ankityadav00159@gmail.com>
2023-02-13 09:35:42 +0530

    add image

--- src/Components/FaceRecoginition/FaceRecoginition.js
+++ src/Components/FaceRecoginition/FaceRecoginition.js
@@ -1,7 +1,12 @@
 import React from 'react'
 
 const FaceRecoginition = () =>{
+    return(
+        <div className='center'>
+        <img alt='sample' src={"https://media.istockphoto.com/photos/unity-of-large-group-of-happy-people-looking-at-camera-picture-id472509548"}/>
 
+        </div>
+    )
 }
 
 export default FaceRecoginition

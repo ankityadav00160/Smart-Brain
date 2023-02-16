065a222381a6ca52b346943ba0d10e6d3a494021
ankityadav00159 <ankityadav00159@gmail.com>
2023-02-16 11:07:44 +0530

    Restore to previous version

--- src/Components/ImageLinkForm/ImageLinkForm.jsx
+++ src/Components/ImageLinkForm/ImageLinkForm.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import './ImageLinkForm.css'
 
-const ImageLinkForm = ({onInputChange,onButtonSumbit}) =>{
+const ImageLinkForm = ({onInputChange}) =>{
     return( 
         <>
             <p className="f3">
@@ -10,8 +10,7 @@ const ImageLinkForm = ({onInputChange,onButtonSumbit}) =>{
             <div className="center">
                 <div className=" form center pa4 br3 shadow-5">
                     <input className="f4 pa2 w-70 center" type= 'text' onChange={onInputChange} />
-                    <button className="w-30 grow f4 link ph3 pv2 dib white bg-light-purple" 
-                    onClick={onButtonSumbit}>Detect</button>
+                    <button className="w-30 grow f4 link ph3 pv2 dib white bg-light-purple" >Detect</button>
                 </div>
             </div>
         </>

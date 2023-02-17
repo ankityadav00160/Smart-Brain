dbb2d9c1c3623015f06a1bafdd98d443c2d1b419
ankityadav00159 <ankityadav00159@gmail.com>
2023-02-17 18:51:34 +0530

    app.js , face recoginition, imagelink

--- src/App.js
+++ src/App.js
@@ -4,17 +4,72 @@ import Navigation from './Components/Navigation/Navigation.js'
 import Logo from './Components/Logo/Logo.js'
 import ImageLinkForm from './Components/ImageLinkForm/ImageLinkForm.jsx'
 import Rank from './Components/Rank/Rank.js'
+import FaceRecoginition from './Components/FaceRecoginition/FaceRecoginition'
+import Learn from './Components/Learn/Learn.js'
 
 class App extends Component {
   constructor() {
     super()
     this.state = {
-      input: '',  
+      input: '',
+      imageUrl: ''  
     }
   }
 
   onInputChange = (event) => {
-    console.log(event.target.value)
+    this.setState({input:event.target.value})
+  }
+
+  onButtonSumbit = () =>{
+    // console.log('click');
+    this.setState({imageUrl: this.state.input});
+    const PAT = 'd271ca98cf5e443a9ca40fe686ff3299';
+    // Specify the correct user_id/app_id pairings
+    // Since you're making inferences outside your app's scope
+    const USER_ID = 'ankit00160';       
+    const APP_ID = 'my-first-application';
+    // Change these to whatever model and image URL you want to use
+    const MODEL_ID = 'face-detection';
+    const MODEL_VERSION_ID = '6dc7e46bc9124c5c8824be4822abe105';    
+    const IMAGE_URL = this.state.input;
+
+    ///////////////////////////////////////////////////////////////////////////////////
+    // YOU DO NOT NEED TO CHANGE ANYTHING BELOW THIS LINE TO RUN THIS EXAMPLE
+    ///////////////////////////////////////////////////////////////////////////////////
+
+    const raw = JSON.stringify({
+        "user_app_id": {
+            "user_id": USER_ID,
+            "app_id": APP_ID
+        },
+        "inputs": [
+            {
+                "data": {
+                    "image": {
+                        "url": IMAGE_URL
+                    }
+                }
+            }
+        ]
+    });
+
+    const requestOptions = {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Authorization': 'Key ' + PAT
+        },
+        body: raw
+    };
+
+    // NOTE: MODEL_VERSION_ID is optional, you can also call prediction with the MODEL_ID only
+    // https://api.clarifai.com/v2/models/{YOUR_MODEL_ID}/outputs
+    // this will default to the latest version_id
+
+    fetch("https://api.clarifai.com/v2/models/" + MODEL_ID + "/versions/" + MODEL_VERSION_ID + "/outputs", requestOptions)
+        .then(response => response.text())
+        .then(result => console.log(result))
+        .catch(error => console.log('error', error));
   }
 
   render(){
@@ -23,8 +78,9 @@ class App extends Component {
       <Navigation/>
       <Logo/>
       <Rank/>
-      <ImageLinkForm onInputChange={this.onInputChange}/>
-      {/* <FaceRecoginition/> */}
+      <ImageLinkForm onInputChange={this.onInputChange} onButtonSumbit={this.onButtonSumbit}/>
+      <FaceRecoginition imageUrl={this.state.imageUrl}/>
+      <Learn/>
     </div>
   );
 }
---
+++ src/Components/FaceRecoginition/FaceRecoginition.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const FaceRecoginition = (imageUrl) =>{
+    return(
+        <div className='center'>
+            <img alt='sample' src={imageUrl}/>
+        </div>
+    )
+}
+
+export default FaceRecoginition
